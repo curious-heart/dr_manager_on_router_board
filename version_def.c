1972aac4ba06c4d62827b53c4428c451ae8df120
curious-heart <onground@163.com>
2024-06-13 14:31:45 +0800

    change the version number to released-version (remove the appended "m" and "l")

--- version_def.c
+++ version_def.c
@@ -16,9 +16,9 @@ const char* g_APP_NAME = "dr_manager";
  * Note 2: when update version number, please update the definition in both case so that both version can be of the latest.
  * */
 #ifdef MANAGE_LCD_AND_TOF_HERE
-const char * g_APP_VER_STR = "038m"; 
+const char * g_APP_VER_STR = "038"; 
 #else
-const char * g_APP_VER_STR = "238m"; 
+const char * g_APP_VER_STR = "238"; 
 #endif
 
 const unsigned char g_SW_VER_NUMBER = 1;
